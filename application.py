b8b377ad342308a0b43249943c048b678f51d803
Marcos Allonso <80725858+mallonso@users.noreply.github.com>
2021-03-15 22:09:49 -0300

    version 4

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA mallonso v3! o/"
+    return "<h1>Hola FIAP!</h1>\mallonso v4! o/"
 
 if __name__ == '__main__':
     # application.run()
